32957d7d00144c1641bcf42249948b5bf2ec4c26
PirateKing55 <itsamrit26@gmail.com>
2023-05-14 17:57:15 +0530

    added zoom feature

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 //
 const Card = (props) => {
   return (
-    <div className="mk-card">
-      <h1>{props.title}</h1>
-      <p>
-        <b>Roll:</b> {props.content}
-      </p>
-      <img src="images/iitk-logo.png" alt="pic" width="60px" />
-    </div>
+      <div className="mk-card">
+        <h1>{props.title}</h1>
+        <p>
+          <b>Roll:</b> {props.content}
+        </p>
+        <img src="images/iitk-logo.png" alt="pic" width="60px" />
+      </div>
   );
 };
 
