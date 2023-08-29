b7bfd48947831be8bdee58b1465434673b81b28e
PirateKing55 <itsamrit26@gmail.com>
2023-08-29 16:00:01 +0530

    image API added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,17 @@
 import StudentsLogo from "./StudentsLogo";
 
 function Card({ studentName, studentRoll }) {
+  let picUrl = `https://oa.cc.iitk.ac.in/Oa/Jsp/Photo/${studentRoll}_0.jpg`;
   return (
     <div className="mk-card">
       <h1>{studentName}</h1>
       <p>
         <b>Roll:</b> {studentRoll}
       </p>
-      <StudentsLogo />
+      {/* <StudentsLogo /> */}
+      <div>
+        <img className="student-logo" src={picUrl} />
+      </div>
     </div>
   );
 }
